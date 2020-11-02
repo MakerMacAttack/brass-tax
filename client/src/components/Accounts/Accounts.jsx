bf7fa20864d3c4e94ce82c045e6e5074a2de3419
Mac May <tmcpbmay@gmail.com>
2020-11-01 19:52:35 -0500

    typo

--- client/src/components/Accounts/Accounts.jsx
+++ client/src/components/Accounts/Accounts.jsx
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AccountCreate from "../../components/AccountCreate/AccountCreate";
+import Account from "../../components/Account/Account";
+import { getAccounts } from "../../services/accounts";
 import "./Accounts.css";
 
 export default function Accounts(props) {
-  const [accounts, setAccounts] = useState([]);
+  const [allAccounts, setAllAccounts] = useState([]);
+
+  useEffect(() => {
+    const fetchAccounts = async () => {
+      const accounts = await getAccounts();
+      setAllAccounts(accounts);
+    };
+    fetchAccounts();
+  }, []);
 
   return (
     <div>
-      <h1>Display Current Accounts (Pick one to see details)</h1>
+      {allAccounts.map((account) => (
+        <Account key={account._id} account={account} />
+      ))}
       <AccountCreate />
     </div>
   );
