71e4ba240ec8489156d801fb1c03ab79b816b049
Mac May <tmcpbmay@gmail.com>
2020-11-01 21:16:39 -0500

    Button toggles between hide and show

--- client/src/components/Accounts/Accounts.jsx
+++ client/src/components/Accounts/Accounts.jsx
@@ -8,6 +8,11 @@ export default function Accounts(props) {
   const [allAccounts, setAllAccounts] = useState([]);
   const [trigger, setTrigger] = useState(true);
   const [total, setTotal] = useState(0);
+  const [listHidden, setListHidden] = useState(true);
+
+  function handleList() {
+    setListHidden((prev) => !prev);
+  }
 
   useEffect(() => {
     const fetchAccounts = async () => {
@@ -18,18 +23,25 @@ export default function Accounts(props) {
   }, [trigger]);
 
   useEffect(() => {
-    const balances = allAccounts.map((account) => account.balance);
-    const sum = balances.reduce((acc, curr) => acc + curr);
-    setTotal(sum);
+    if (allAccounts.length > 0) {
+      const balances = allAccounts.map((account) => account.balance);
+      const sum = balances.reduce((acc, curr) => acc + curr);
+      setTotal(sum);
+    }
   }, [allAccounts]);
 
   return (
     <div>
       <h2>Your total current savings is: {total}</h2>
-      {allAccounts.map((account) => (
-        <Account key={account._id} account={account} />
-      ))}
-      <AccountCreate set={setTrigger} />
+      <button onClick={handleList}>
+        {listHidden ? "View Accounts" : "Hide Accounts"}
+      </button>
+      <div id="account-list">
+        {allAccounts.map((account) => (
+          <Account key={account._id} account={account} />
+        ))}
+        <AccountCreate set={setTrigger} />
+      </div>
     </div>
   );
 }
